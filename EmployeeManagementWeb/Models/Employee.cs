e784a01f4b0d16253f6237e705ce1f28101895c3
Rumali0131 <rumali0131@gmail.com>
2022-05-19 00:52:02 +0530

    Create Employee model and add data annotations

---
+++ EmployeeManagementWeb/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementWeb.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public String Position { get; set; }
+        public int Salary { get; set; }
+        public String? ContactNo { get; set; }
+    }
+}
